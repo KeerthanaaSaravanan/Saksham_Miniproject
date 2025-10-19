635c0d7e82cb8373bc2da0e5d0b2341b972ff636
Keerthana Saravanan <keerthanasaravanankt15@gmail.com>
2025-10-19 07:32:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6947918129-953de",
+  "appId": "1:617944914755:web:471116e2ace1d8f8f3edf6",
+  "apiKey": "AIzaSyCIVlssX9FI691fpISwDwet2stU4D3NzkI",
+  "authDomain": "studio-6947918129-953de.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "617944914755"
+};
